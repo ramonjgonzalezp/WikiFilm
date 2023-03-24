0148a257cc0813cf3565a10302c40c53951511b3
ramonjgonzalezp <ramonjgonzalezp@gmail.com>
2023-03-24 21:25:08 +0100

    Update WikiFilm.py

--- WikiFilm.py
+++ WikiFilm.py
@@ -2,13 +2,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
 # import smtplib
 # from email.mime.text import MIMEText
 # from email.mime.multipart import MIMEMultipart
 # from email.mime.application import MIMEApplication
 
 # Definir las variables necesarias
-busqueda = "Universos de películas"
+busqueda = "Universos en las películas"
 url_wikipedia = "https://es.wikipedia.org/wiki/Wikipedia:Portada"
 email_emisor = "pineapple.dev.rg@gmail.com"
 contrasena_emisor = "jysxysmfcwegojjn"
@@ -16,7 +17,7 @@ email_destinatario = "ramonjgonzalezp@gmail.com"
 
 # Abrir Google Chrome, maximizar la ventana y navegar a Wikipedia
 driver = webdriver.Chrome()
-driver.maximize_window()
+# driver.maximize_window()
 driver.get(url_wikipedia)
 
 # Buscar la barra de búsqueda y escribir la búsqueda
@@ -27,15 +28,28 @@ barra_busqueda.send_keys(Keys.RETURN)
 # Esperar a que se carguen los resultados
 driver.implicitly_wait(2)
 
-# Encontrar los tres primeros resultados y obtener sus títulos
-resultados_de_la_busqueda = driver.find_elements(By.XPATH, "//div[@id='wikipedia-search-result-link']/a")[:3]
-links = []
-for resultado in resultados_de_la_busqueda:
-    url_data = resultado.get_attribute("href")
-    links.append(url_data)
-    print(url_data)
-print(len(links))
-print(links)
+# Encontrar los tres primeros resultados y mostrarlos en la consola
+primer_resultado_de_la_busqueda = driver.find_element(By.XPATH, "//*[@id='mw-content-text']/div[4]/div[2]/ul/li[1]/table/tbody/tr/td[2]/div[1]")
+segundo_resultado_de_la_busqueda = driver.find_element(By.XPATH, "//*[@id='mw-content-text']/div[4]/div[2]/ul/li[2]/table/tbody/tr/td[2]/div[1]")
+tercer_resultado_de_la_busqueda = driver.find_element(By.XPATH, "//*[@id='mw-content-text']/div[4]/div[2]/ul/li[3]/table/tbody/tr/td[2]/div[1]")
+print("Los tres primeros resultados son:")
+print(primer_resultado_de_la_busqueda.text)
+print(segundo_resultado_de_la_busqueda.text)
+print(tercer_resultado_de_la_busqueda.text)
+
+# Encontrar todos los resultados e imprimir por consola
+resultados_de_la_búsqueda = driver.find_elements(By.XPATH, "//*[@id='mw-content-text']/div[4]/div[2]/ul")
+for resultado in resultados_de_la_búsqueda:
+    print("Resultados:")
+    print(resultado.text)
+
+# links = []
+# for resultado in resultados_de_la_busqueda:
+#     url_data = resultado.get_attribute("href")
+#     links.append(url_data)
+#     print(url_data)
+# print(len(links))
+# print(links)
 
 # Cerrar el navegador y crear el correo electrónico
 driver.quit()
